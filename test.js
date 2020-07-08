dc51486e286493eb72457fba5300d46a7dea990c
kai.zhu <kai.zhu@windebt.com>
2020-07-08 17:29:11 -0500

    node

--- test.js
+++ test.js
@@ -174,8 +174,8 @@
     let base64urlFromBuffer;
     let base64urlToBuffer;
     let cryptoDecryptBrowser;
-    //!! let cryptoDecryptNode;
     let cryptoEncryptBrowser;
+    //!! let cryptoKeyWrapNode;
     let cryptoValidateHeader;
     let runMe;
     assertJsonEqual = local.assertJsonEqual;
@@ -183,13 +183,18 @@
     base64urlFromBuffer = function (buf) {
         let base64url;
         let ii;
-        base64url = "";
-        ii = 0;
-        while (ii < buf.byteLength) {
-            base64url += String.fromCharCode(buf[ii]);
-            ii += 1;
+        if (typeof globalThis.btoa === "function") {
+            base64url = "";
+            ii = 0;
+            while (ii < buf.byteLength) {
+                base64url += String.fromCharCode(buf[ii]);
+                ii += 1;
+            }
+            base64url = globalThis.btoa(base64url);
+        } else {
+            base64url = buf.toString("base64");
         }
-        return globalThis.btoa(base64url).replace((
+        return base64url.replace((
             /\+/g
         ), "-").replace((
             /\//g
@@ -198,15 +203,19 @@
         ), "");
     };
     base64urlToBuffer = function (base64url) {
-        return Uint8Array.from(globalThis.atob(base64url.replace((
+        base64url = base64url.replace((
             /-/g
         ), "+").replace((
             /_/g
         ), "/").replace((
             /\=*?$/
-        ), "")), function (chr) {
-            return chr.charCodeAt(0);
-        });
+        ), "");
+        if (typeof globalThis.atob === "function") {
+            return Uint8Array.from(globalThis.atob(base64url), function (chr) {
+                return chr.charCodeAt(0);
+            });
+        }
+        return Buffer.from(base64url, "base64");
     };
     cryptoDecryptBrowser = async function (kek, jwe) {
         let cek;
@@ -298,54 +307,6 @@
             + "." + base64urlFromBuffer(tmp.subarray(-16))
         );
     };
-    //!! cryptoDecryptNode = async function (kek, plaintext, header, cek, iv) {
-        //!! let crypto;
-        //!! let tmp;
-        //!! crypto = globalThis.crypto;
-        //!! header = header || {
-            //!! "alg": "A256KW",
-            //!! "enc": "A256GCM"
-        //!! };
-        //!! kek = base64urlToBuffer(kek);
-        //!! cek = base64urlToBuffer(cek || base64urlFromBuffer(
-            //!! crypto.getRandomValues(new Uint8Array(
-                //!! header.enc !== "A256GCM"
-                //!! ? 16
-                //!! : 32
-            //!! ))
-        //!! ));
-        //!! // validate header
-        //!! cryptoValidateHeader(header, kek, cek, 0);
-        //!! kek = await crypto.subtle.importKey("raw", kek, "AES-KW", false, [
-            //!! "wrapKey"
-        //!! ]);
-        //!! header = base64urlFromBuffer(
-            //!! new TextEncoder().encode(JSON.stringify(header))
-        //!! );
-        //!! iv = iv || base64urlFromBuffer(
-            //!! crypto.getRandomValues(new Uint8Array(12))
-        //!! );
-        //!! cek = await crypto.subtle.importKey("raw", cek, {
-            //!! name: "AES-GCM"
-        //!! }, true, [
-            //!! "encrypt"
-        //!! ]);
-        //!! tmp = new Uint8Array(await crypto.subtle.encrypt({
-            //!! additionalData: new TextEncoder().encode(header),
-            //!! iv: base64urlToBuffer(iv),
-            //!! name: "AES-GCM"
-        //!! }, cek, new TextEncoder().encode(plaintext)));
-        //!! cek = base64urlFromBuffer(new Uint8Array(
-            //!! await crypto.subtle.wrapKey("raw", cek, kek, "AES-KW")
-        //!! ));
-        //!! return (
-            //!! header
-            //!! + "." + cek
-            //!! + "." + iv
-            //!! + "." + base64urlFromBuffer(tmp.subarray(0, -16))
-            //!! + "." + base64urlFromBuffer(tmp.subarray(-16))
-        //!! );
-    //!! };
     cryptoValidateHeader = function (header, kek, cek, cekPadding) {
         assertOrThrow((
             (header.alg === "A128KW" && header.enc === "A128GCM")
@@ -405,6 +366,7 @@
             // tag - Authentication tag
             + "ER7MWJZ1FBI_NKvn7Zb1Lw"
         ));
+        // cek = "aY5_Ghmk9KxWPBLu_glx1w";
         myPlaintext = await cryptoDecryptBrowser(
             "GZy6sIZ6wl9NJOKB-jnmVQ",
             myJwe
@@ -442,5 +404,193 @@
         myPlaintext = await cryptoDecryptBrowser(myKek, myJwe);
         console.log("decrypted jwe - " + myPlaintext);
     };
-    runMe();
+    await runMe();
+
+
+    //!! cryptoKeyWrapNode = function (KK, RR, mode) {
+    //!! /*
+     //!! * this function will wrap/uwrap <KK> with given <RR>
+     //!! * https://tools.ietf.org/html/rfc7516#appendix-A.3.3
+        //!! 2.2.1 Key Wrap
+        //!! https://tools.ietf.org/html/rfc3394#section-2.2.1
+            //!! Inputs: Plaintext, n 64-bit values {P1, P2, ..., Pn}, and
+                //!! Key, K (the KEK).
+            //!! Outputs: Ciphertext, (n+1) 64-bit values {C0, C1, ..., Cn}.
+            //!! 1) Initialize variables.
+                //!! Set A = IV, an initial value (see 2.2.3)
+                //!! For i = 1 to n
+                    //!! R[i] = P[i]
+            //!! 2) Calculate intermediate values.
+                //!! For j = 0 to 5
+                    //!! For i = 1 to n
+                        //!! B = AES(K, A | R[i])
+                        //!! A = MSB(64, B) ^ t where t = (n*j)+i
+                        //!! R[i] = LSB(64, B)
+            //!! 3) Output the results.
+                //!! Set C[0] = A
+                //!! For i = 1 to n
+                    //!! C[i] = R[i]
+        //!! 2.2.2 Key Unwrap
+        //!! https://tools.ietf.org/html/rfc3394#section-2.2.2
+            //!! Inputs: Ciphertext, (n+1) 64-bit values {C0, C1, ..., Cn}, and
+                //!! Key, K (the KEK).
+            //!! Outputs: Plaintext, n 64-bit values {P0, P1, K, Pn}.
+            //!! 1) Initialize variables.
+                //!! Set A = C[0]
+                //!! For i = 1 to n
+                    //!! R[i] = C[i]
+            //!! 2) Compute intermediate values.
+                //!! For j = 5 to 0
+                    //!! For i = n to 1
+                        //!! B = AES-1(K, (A ^ t) | R[i]) where t = n*j+i
+                        //!! A = MSB(64, B)
+                        //!! R[i] = LSB(64, B)
+            //!! 3) Output results.
+                //!! For i = 1 to n
+                    //!! P[i] = R[i]
+     //!! */
+        //!! let AA;
+        //!! let BB;
+        //!! let crypto;
+        //!! let ii;
+        //!! let iv;
+        //!! let jj;
+        //!! let loop;
+        //!! let nn;
+        //!! let tt;
+        //!! // init var
+        //!! AA = new Uint8Array(32);
+        //!! ii = 0;
+        //!! iv = new Uint8Array(16);
+        //!! nn = 4;
+        //!! crypto = (
+            //!! mode === "unwrap"
+            //!! ? require("crypto").createDecipheriv
+            //!! : require("crypto").createCipheriv
+        //!! );
+        //!! // init loop
+        //!! loop = function () {
+            //!! // AA xor tt
+            //!! if (mode === "unwrap") {
+                //!! tt = nn * jj + ii;
+                //!! AA[4] ^= ((tt >>> 24) & 0xff);
+                //!! AA[5] ^= ((tt >> 16) & 0xff);
+                //!! AA[6] ^= ((tt >> 8) & 0xff);
+                //!! AA[7] ^= (tt & 0xff);
+            //!! }
+            //!! // init RR
+            //!! AA[8] = RR[8 * ii];
+            //!! AA[9] = RR[8 * ii + 1];
+            //!! AA[10] = RR[8 * ii + 2];
+            //!! AA[11] = RR[8 * ii + 3];
+            //!! AA[12] = RR[8 * ii + 4];
+            //!! AA[13] = RR[8 * ii + 5];
+            //!! AA[14] = RR[8 * ii + 6];
+            //!! AA[15] = RR[8 * ii + 7];
+            //!! // encrypt / decrypt RR
+            //!! BB = crypto("aes-128-cbc", KK, iv);
+            //!! BB.setAutoPadding(false);
+            //!! BB = Buffer.concat([
+                //!! BB.update(AA), BB.final()
+            //!! ]);
+            //!! // update RR
+            //!! AA[0] = BB[0];
+            //!! AA[1] = BB[1];
+            //!! AA[2] = BB[2];
+            //!! AA[3] = BB[3];
+            //!! AA[4] = BB[4];
+            //!! AA[5] = BB[5];
+            //!! AA[6] = BB[6];
+            //!! AA[7] = BB[7];
+            //!! RR[8 * ii + 0] = BB[8];
+            //!! RR[8 * ii + 1] = BB[9];
+            //!! RR[8 * ii + 2] = BB[10];
+            //!! RR[8 * ii + 3] = BB[11];
+            //!! RR[8 * ii + 4] = BB[12];
+            //!! RR[8 * ii + 5] = BB[13];
+            //!! RR[8 * ii + 6] = BB[14];
+            //!! RR[8 * ii + 7] = BB[15];
+            //!! // AA xor tt
+            //!! if (mode !== "unwrap") {
+                //!! tt = nn * jj + ii;
+                //!! AA[4] ^= ((tt >>> 24) & 0xff);
+                //!! AA[5] ^= ((tt >> 16) & 0xff);
+                //!! AA[6] ^= ((tt >> 8) & 0xff);
+                //!! AA[7] ^= (tt & 0xff);
+            //!! }
+        //!! };
+        //!! if (mode === "unwrap") {
+            //!! AA[0] = RR[0];
+            //!! AA[1] = RR[1];
+            //!! AA[2] = RR[2];
+            //!! AA[3] = RR[3];
+            //!! AA[4] = RR[4];
+            //!! AA[5] = RR[5];
+            //!! AA[6] = RR[6];
+            //!! AA[7] = RR[7];
+            //!! jj = 5;
+            //!! while (0 <= jj) {
+                //!! ii = nn;
+                //!! while (1 <= ii) {
+                    //!! loop();
+                    //!! ii -= 1;
+                //!! }
+                //!! jj -= 1;
+            //!! }
+            //!! return RR.slice(8);
+        //!! }
+        //!! BB = RR;
+        //!! RR = new Uint8Array(BB.length + 8);
+        //!! ii = 0;
+        //!! while (ii < BB.length) {
+            //!! RR[ii + 8] = BB[ii];
+            //!! ii += 1;
+        //!! }
+        //!! AA[0] = 0xa6;
+        //!! AA[1] = 0xa6;
+        //!! AA[2] = 0xa6;
+        //!! AA[3] = 0xa6;
+        //!! AA[4] = 0xa6;
+        //!! AA[5] = 0xa6;
+        //!! AA[6] = 0xa6;
+        //!! AA[7] = 0xa6;
+        //!! jj = 0;
+        //!! while (jj <= 5) {
+            //!! ii = 1;
+            //!! while (ii <= nn) {
+                //!! loop();
+                //!! ii += 1;
+            //!! }
+            //!! jj += 1;
+        //!! }
+        //!! RR[0] = AA[0];
+        //!! RR[1] = AA[1];
+        //!! RR[2] = AA[2];
+        //!! RR[3] = AA[3];
+        //!! RR[4] = AA[4];
+        //!! RR[5] = AA[5];
+        //!! RR[6] = AA[6];
+        //!! RR[7] = AA[7];
+        //!! return RR;
+    //!! };
+    //!! cryptoKeyWrapNode = function (kek, cek) {
+    //!! };
+    runMe = async function () {
+        let cek;
+        let kek;
+        let nn;
+        let rr;
+        if (local.isBrowser) {
+            return;
+        }
+        kek = base64urlToBuffer("GZy6sIZ6wl9NJOKB-jnmVQ");
+        cek = base64urlToBuffer("CBI6oDw8MydIx1IBntf_lQcw2MmJKIQx");
+        debugInline(kek);
+        debugInline(cek);
+        //!! cek = base64urlFromBuffer(
+            //!! cryptoKeyWrapNode(kek, cek, "unwrap")
+        //!! );
+        debugInline(cek);
+    };
+    await runMe();
 }(globalThis.globalLocal));
